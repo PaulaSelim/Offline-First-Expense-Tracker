04ad62ce75475d28ad2ec9f8304c2af663b5404e
Mina George <mina.george.girgis@gmail.com>
2025-08-06 12:56:48 +0300

    refactor: enhance expense forms

--- src/app/pages/expense-edit/expense-edit.ts
+++ src/app/pages/expense-edit/expense-edit.ts
@@ -24,6 +24,7 @@ import { Group, GroupMember } from '../../core/api/groupApi/groupApi.model';
 import {
   Expense,
   ExpenseUpdateRequest,
+  Participant,
 } from '../../core/api/expenseApi/expenseApi.model';
 import { categories, Category } from '../expense/expense.model';
 
@@ -127,10 +128,12 @@ export class ExpenseEdit implements OnInit {
       this.expenseForm.patchValue({
         title: expense.title,
         amount: expense.amount,
-        payer_id: expense.payer_id,
+        payer_id: expense.payer?.id || '',
         category: expense.category,
         date: expense.date,
-        participants_id: expense.participants || [],
+        participants_id: (expense.participants || []).map(
+          (p: Participant) => p.id,
+        ),
       });
     }
   }
